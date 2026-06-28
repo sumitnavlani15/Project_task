ad1ddd38f12af14655a6990377e9a8a800172eaf
Sumit Navlani <sumitnavlani15@gmail.com>
2026-06-28 13:33:14 +0530

    Fix Express app initialization

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require("mongoose");
